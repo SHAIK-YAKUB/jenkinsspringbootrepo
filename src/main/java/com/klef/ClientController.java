24ddd0765f5f727eebf597e6a8524b5aee94aa55
SHAIK-YAKUB <2300030833@kluniversity.in>
2025-08-07 16:44:22 +0530

    spring boot project v1.0

---
+++ src/main/java/com/klef/ClientController.java
@@ -0,0 +1,19 @@
+package com.klef;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientController {
+	@GetMapping("/")
+	public String home()
+	{
+		return "CI/CD and cloud Devops";
+	}
+	@GetMapping("/demo")
+	public String demo()
+	{
+		return "jenkins Demo";
+	}
+
+}
